dea921d3c1dc57da37f21d10d2ffe8e145f787dd
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-09-27 19:32:44 -0500

    added my profile

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -68,6 +68,9 @@ public class SpaDayController {
 
     @PostMapping(value="")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
 
         ArrayList<String> facials = new ArrayList<String>();
         facials.add("Microdermabrasion");
