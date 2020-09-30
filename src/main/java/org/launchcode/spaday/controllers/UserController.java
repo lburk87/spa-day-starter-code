157c6d6f876e1a169cbf4b3f4c64484dba7d7449
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-09-30 18:27:54 -0500

    also took UserController

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,29 +1,34 @@
 package org.launchcode.spaday.controllers;
 
-import org.apache.catalina.User;
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("user/add")
+@RequestMapping("user")
 public class UserController {
 
-    @GetMapping("add")
-    public String displayAddUserForm(Model model) {
+    @GetMapping("/add")
+    public String displayAddUserForm() {
         return "user/add";
     }
 
-    @PostMapping("add")
+    @PostMapping
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute(user);
-        if (verify == user.getPassword()) {
-            return "user/index.html";
-        } else {
-            return "redirect";
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
         }
+        else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+
     }
+
+
 }
