97580a0f19517fe5023f45f5a65cc1ab4156be30
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-09-30 18:00:52 -0500

    added processAddUserForm

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,9 +1,29 @@
 package org.launchcode.spaday.controllers;
 
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("user/add")
 public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm(Model model) {
+        return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute(user);
+        if (verify == user.getPassword()) {
+            return "user/index.html";
+        } else {
+            return "redirect";
+        }
+    }
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,4 +1,34 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+
 public class User {
+
+    private static String username;
+    private Email email;
+    private String password;
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        User.username = username;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
